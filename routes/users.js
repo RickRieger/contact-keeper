be6831bd16de3d7bd86e0e64077af5fbb0229778
Rick Rieger <rickrieger36251@gmail.com>
2021-11-03 13:51:20 -0400

    backend finished

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
-
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const config = require('config');
 
 // @route  POST api/users
 // @desc   Register a user
@@ -30,7 +31,7 @@ router.post(
         return res.status(400).json({ msg: 'User already exists' });
       }
 
-      user = newUser({ name, email, password });
+      user = new User({ name, email, password });
 
       const salt = await bcrypt.genSalt(10);
 
@@ -38,7 +39,23 @@ router.post(
 
       await user.save();
 
-      res.send('user saved');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
